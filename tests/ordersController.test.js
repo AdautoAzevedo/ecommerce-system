d0587b7464c346b6cc5b53f8f0c3dac6b27a1e21
Adauto Azevêdo <adautofreitasdeazevedo@gmail.com>
2024-02-05 16:22:18 -0300

    Added test for ordersController

---
+++ tests/ordersController.test.js
@@ -0,0 +1,69 @@
+const { processOrder } = require('../controllers/ordersController');
+const Order = require('../model/order');
+const Cart = require('../model/cart');
+const CatItem = require('../model/cartItem');
+const Product = require('../model/product');
+const CartItem = require('../model/cartItem');
+
+jest.mock('../model/order');
+jest.mock('../model/cart');
+jest.mock('../model/cartItem');
+jest.mock('../model/product');
+
+describe('processOrder controller', () => {
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it('should create order successfully', async () => {
+        const req = {
+            user: {user_id: 1}
+        };
+
+        const res = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn()
+        };
+
+        const mockCartItem = {
+            product_id: 1,
+            product: {product_name: 'Some Product'},
+            quantity: 2
+        };
+
+        const mockCart = {
+            cartItems: [mockCartItem],
+            totalPrice: 20
+        };
+
+        Cart.findOne.mockResolvedValue(mockCart);
+        Order.create.mockResolvedValue({order_id: 1, user_id: 1, totalPrice: 20});
+
+        await processOrder(req, res);
+
+        expect(Cart.findOne).toHaveBeenCalledWith({
+            where: {user_id: 1},
+            include: [{model: CartItem, include:[Product]}],
+        });
+
+        expect(Order.create).toHaveBeenCalledWith({
+            user_id: 1,
+            totalPrice: 20
+        });
+
+        expect(res.status).toHaveBeenCalledWith(201);
+        expect(res.json).toHaveBeenCalledWith({
+            message: 'Order placed sucessfully',
+            order: {
+                order_id: 1,
+                user_id: 1,
+                total_price: 20,
+                order_items: [{
+                    product_id: 1,
+                    product_name: 'Some Product',
+                    quantity: 2
+                }],
+            }
+        });
+    });
+})

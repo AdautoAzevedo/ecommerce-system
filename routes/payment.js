8ff006c356e4f23732da364614c7a220682fba24
AdautoAzevedo <adautofreitasdeazevedo@gmail.com>
2024-08-12 13:43:40 -0300

    Updated payment route

--- routes/payment.js
+++ routes/payment.js
@@ -4,6 +4,6 @@ const paymentController = require("../controllers/paymentController");
 const verifyJWT = require("../middleware/verifyJWT");
 
 router.route('/')
-    .post(verifyJWT, paymentController.createPaymentIntent);
+    .post(paymentController.createPaymentIntent);
 
 module.exports = router;
